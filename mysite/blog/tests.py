f0edcdfef2536cfaf65a53b095768e49db8c9884
varin-pierre <pvarin@student.42.fr>
2016-05-27 23:19:15 +0200

    friday 26 mai

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,8 +1,7 @@
+import datetime
 from django.test import TestCase
 from django.utils import timezone
 from .models import Post
 
 # Create your tests here.
 class PostTestCase(TestCase):
-    def edited_date_before_post(self):
-        self.assertEqual(fake_test(), False)
