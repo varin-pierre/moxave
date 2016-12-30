4a709eb2d58df5f17e24518e4f2bb0044b963dbb
varin-pierre <pvarin@student.42.fr>
2016-12-30 18:40:38 +0100

    skeleton

--- website/hello.py
+++ website/hello.py
@@ -3,15 +3,32 @@ from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('WEBSITE_SETTING', silent=True)
+app.config.from_envvar('WEBSITE_SETTINGS', silent=True)
 
 @app.route('/')
 def index():
-    return render_template('inidex.html')
-#'Flask is running! modify'
+    return render_template('index.html')
 
+@app.route('/sepia')
+def sepia():
+    return render_template('sepia.html')
+
+@app.route('/monengagement')
+def monengagement():
+    return render_template('monengagement.html')
+
+@app.route('/42')
+def fortytow():
+    return render_template('42.html')
+
+@app.route('/photo')
+def photo():
+    return render_template('photo.html')
+
+@app.route('/source')
+def source():
+    return render_template('source.html')
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
     app.run()
-
